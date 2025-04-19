98c3f64800c42d29528b16577f893f04d24f313f
Ken Karlson <javarecords@mail.ru>
2025-04-19 17:14:14 +0300

    Add Renderer

--- src/main.js
+++ src/main.js
@@ -3,3 +3,15 @@ import { Render } from "./components/Render";
 import { Camera } from "./components/Camera";
 import { Scene } from "./components/Scene";
 import { player } from "./components/Player";
+import "./style.css";
+
+//Create variables
+const scene = new THREE.Scene();
+const camera = Camera();
+const renderer = Renderer();
+//const camera = new THREE.PerspectiveCamera();
+
+//Initialse
+scene.add(player);
+scene.add(camera);
+renderer.render(scene, Camera.camera);
